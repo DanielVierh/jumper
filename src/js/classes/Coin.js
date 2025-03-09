0a85459e81577c5da0becb477556c3c834aad51d
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-03-09 17:40:36 +0100

    use mutation

--- src/js/classes/Coin.js
+++ src/js/classes/Coin.js
@@ -1,32 +1,21 @@
-export class Coin {
+import { CanvasObject } from "./_CanvasObject.js";
+
+export class Coin extends CanvasObject {
     constructor(pos_x, pos_y, width, height, imageSrc) {
-        this.pos_x = pos_x;
-        this.pos_y = pos_y;
-        this.width = width;
-        this.height = height;
+        super(pos_x, pos_y, width, height, imageSrc)
         this.velocity_x = Math.floor(Math.random() * (-1 + 4)) - 4;
         this.gravity = 0;
         this.image = new Image();
         this.image.src = imageSrc;
     }
 
-    update(coins) {
-        this.pos_x += this.velocity_x;
-
-        if (this.pos_x <= -50) {
-            this.pos_x = -50;
-            coins.splice(0, 1);
-            this.velocity_x = 0;
-        }
+    update(obj_arr) {
+        super.update(obj_arr);
     }
 
     draw(ctx) {
-        ctx.drawImage(
-            this.image,
-            this.pos_x,
-            this.pos_y,
-            this.width,
-            this.height
-        );
+        super.draw(ctx);
     }
 }
+
+
