0b7fb41f866a368fed658fbba06f1c9f5a72ced3
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-03-09 22:14:13 +0100

    fix - remove origin class

--- src/js/classes/Coin.js
+++ src/js/classes/Coin.js
@@ -1,21 +1,32 @@
-import { CanvasObject } from "./_CanvasObject.js";
-
-export class Coin extends CanvasObject {
+export class Coin {
     constructor(pos_x, pos_y, width, height, imageSrc) {
-        super(pos_x, pos_y, width, height, imageSrc)
+        this.pos_x = pos_x;
+        this.pos_y = pos_y;
+        this.width = width;
+        this.height = height;
         this.velocity_x = Math.floor(Math.random() * (-1 + 4)) - 4;
         this.gravity = 0;
         this.image = new Image();
         this.image.src = imageSrc;
     }
 
-    update(obj_arr) {
-        super.update(obj_arr);
+    update(coins) {
+        this.pos_x += this.velocity_x;
+
+        if (this.pos_x <= -50) {
+            this.pos_x = -50;
+            coins.splice(0, 1);
+            this.velocity_x = 0;
+        }
     }
 
     draw(ctx) {
-        super.draw(ctx);
+        ctx.drawImage(
+            this.image,
+            this.pos_x,
+            this.pos_y,
+            this.width,
+            this.height
+        );
     }
 }
-
-
